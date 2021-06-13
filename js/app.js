7f34088e806ef5c466308a958a073c881b1150db
ckunakom <ckunakom@gmail.com>
2021-06-13 15:13:53 -0500

    Added result number text

--- js/app.js
+++ js/app.js
@@ -22,7 +22,15 @@ d3.json("./data/clean_top_tracks.json").then(function(trackData) {
         row.append("td").html(`<a href="${data.song_url}">${data.song_url}</a>`);
 
     });
- 
+
+    // Get the number of result
+    var length = trackData.length;
+
+    window.addEventListener('load', (event) => {
+        var result = `There are ${length} result(s).`;
+        document.getElementById("result").innerHTML = result;
+    });
+    
 // Error handler: log error to console when loading data
 }).catch(function(error) {
 console.log(error);
@@ -42,5 +50,59 @@ function filterEvent() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
+    // Select the input element and get the raw HTML node; then get value property of input element
+    var inputTitle = d3.select("#song-title");
+    var inputTitleValue = inputTitle.property("value"); // let's lowercase the input too
 
 };
+
+// Homework solution -------------------------------------------------------------
+
+// // Keep track of all filters
+// var filters = {};
+
+// function updateFilters() {
+
+//     // Save the element, value, and id of the filter that was changed
+//     var changedElement = d3.select(this).select("input");
+//     var elementValue = changedElement.property("value");
+//     var filterId = changedElement.attr("id");
+
+//     // If a filter value was entered then add that filterId and value to the filters list
+//     if (elementValue) {
+//         filters[filterId] = elementValue;
+//     }
+//     // Otherwise, clear that filter from the filters object
+//     else {
+//         delete filters[filterId];
+//     }
+
+//     // Call function to apply all filters and rebuild the table
+//     filterTable()
+
+// }
+
+// function filterTable() {
+
+//     // Set the filteredData to the tableData
+//     let filteredData = tableData;
+
+//     // Loop through all of the filters and keep any data that matches the filter values
+//     Object.entries(filters).forEach(([key, value]) => {
+//         filteredData = filteredData.filter(row => row[key] === value);
+//     });
+
+//     // Rebuild the table using the fitlered data
+//     buildTable(filteredData);
+
+// }
+
+// function buildTable () {
+//     // uhhhhhhhhhhhhhhh what goes here....
+// }
+
+// // Attach an event to listen for changes to each filter
+// d3.selectAll(".filter").on("change", updateFilters);
+
+// // Build the table when the page loads
+// buildTable(tableData);
